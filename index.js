8685aa1185a42b31b373f5e830bfaddb57188a25
Martvie <marviedias@gmail.com>
2022-04-07 14:52:21 -0300

    Page Function

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+
 let somar = function(num1, num2){
     let resultado = num1 + num2
     return `A soma entre ${num1} e ${num2} é: ${resultado}`
@@ -15,14 +16,22 @@ let multiplicar = function(num1, num2){
 
 let dividir = function(num1, num2){
     let resultado = num1 / num2
-    if (resultado % 1 != 0){
+    if (resultado % 1 != resultado){
         resultado.toFixed(2)
     }
     return `A Divisão entre ${num1} e ${num2} é: ${resultado}`
 }
 
+function calcular (){
+    let num1 = Number(document.getElementById('number1').value)
+    let num2 = Number(document.getElementById('number2').value)
 
-console.log(somar(2, 4))
-console.log(subtrair(2, 4))
-console.log(multiplicar(2, 4))
-console.log(dividir(2, 4))
+    const areaDeCalculo = document.getElementById('resultados')
+    
+    console.log(typeof(num1))
+
+    areaDeCalculo.innerHTML = `<p>${somar(num1, num2)}</p>`
+    areaDeCalculo.innerHTML += `<p>${subtrair(num1, num2)}</p>`
+    areaDeCalculo.innerHTML += `<p>${multiplicar(num1, num2)}</p>`
+    areaDeCalculo.innerHTML += `<p>${dividir(num1, num2)}</p>`
+}
